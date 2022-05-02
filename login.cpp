d2026dc76c92bb2e1757d373c214b90d456c1a1a
ajmazzie <ajmazzie@iastate.edu>
2022-05-02 12:20:46 -0500

    adding functions

--- login.cpp
+++ login.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool userLoggedIn = false;
 
 typedef struct login
 {
@@ -20,7 +25,7 @@ void Usage(char *filename)
 }
 
 /* Returns 0 if username is found, 1 if username is not found */
-int searchForUsername(char *fname, char *str)
+int searchForUsername(char *fname, string accountUsername)
 {
     FILE *fp;
     int line_num = 1;
@@ -40,10 +45,10 @@ int searchForUsername(char *fname, char *str)
 
     while (fgets(temp, 512, fp) != NULL)
     {
-        if ((strstr(temp, str)) != NULL)
+        if ((strstr(temp, accountUsername.c_str())) != NULL)
         {
-            printf("A match found on line: %d\n", line_num);
-            printf("\n%s\n", temp);
+            // printf("A match found on line: %d\n", line_num);
+            // printf("\n%s\n", temp);
             find_result++;
         }
         line_num++;
@@ -62,7 +67,7 @@ int searchForUsername(char *fname, char *str)
 }
 
 /* Returns 0 if password is found, 1 if password is not found */
-int searchForPassword(char *fname, char *str)
+int searchForPassword(char *fname, string userPassword)
 {
     FILE *fp;
     int line_num = 1;
@@ -82,10 +87,10 @@ int searchForPassword(char *fname, char *str)
 
     while (fgets(temp, 512, fp) != NULL)
     {
-        if ((strstr(temp, str)) != NULL)
+        if ((strstr(temp, userPassword.c_str())) != NULL)
         {
-            printf("A match found on line: %d\n", line_num);
-            printf("\n%s\n", temp);
+            // printf("A match found on line: %d\n", line_num);
+            // printf("\n%s\n", temp);
             find_result++;
         }
         line_num++;
@@ -101,36 +106,6 @@ int searchForPassword(char *fname, char *str)
         fclose(fp);
     }
     return (0);
-}
-
-int main(int argc, char *argv[])
-{
-    char option;
-
-    while (1)
-    {
-        printf("\n    Main Menu\n----------------\n'1' to Register\n'2' to Login\n'Q' to Quit\n----------------\n");
-        option = getchar();
-
-        /* 49 == '1' */
-        if (option == 49)
-        {
-            system("CLS");
-            registration();
-        }
-        /* 50 == '2' */
-        else if (option == 50)
-        {
-            system("CLS");
-            login();
-        }
-        /* 81 == 'Q' */
-        else if (option == 81)
-        {
-            break;
-        }
-    }
-    return 0;
 }
 
 int getNewAccountID()
@@ -161,6 +136,8 @@ void login(void)
     FILE *credentialsFile;
     int usernameFound = 1;
     char fileName[10] = "login.txt";
+    string accountName;
+    string userPassword;
 
     credentialsFile = fopen(fileName, "r");
 
@@ -170,32 +147,37 @@ void login(void)
         exit(1);
     }
 
-    account_t userAccount;
-
     printf("\nPlease Enter your login credentials below\n\n");
-    printf("Enter Username: ");
-    scanf("%s", userAccount.username);
-    printf("Enter Password: ");
-    scanf("%s", userAccount.password);
+    printf("\nEnter Username: ");
+    cin >> accountName; // get user input from the keyboard
+    printf("\nEnter Password: ");
+    cin >> userPassword; // get user input from the keyboard
 
     /* Search credentialsFile for username */
-    usernameFound = searchForUsername(fileName, userAccount.username);
+    usernameFound = searchForUsername(fileName, accountName);
 
     if (usernameFound == 0)
     {
-        int passwordFound = searchForPassword(fileName, userAccount.password);
+        int passwordFound = searchForPassword(fileName, userPassword);
         if (passwordFound == 0)
         {
-            printf("\nUser is logged in!");
+            // printf("\nUser is logged in!");
+            userLoggedIn = true;
         }
         else
         {
             printf("\nPassword incorrect!");
+            printf("\nPress enter to continue...");
+            getchar();
+            getchar();
         }
     }
-    else
+    else // Username not found!
     {
         printf("Username not found!");
+        printf("\nPress enter to continue...");
+        getchar();
+        getchar();
     }
 
     fclose(credentialsFile);
@@ -216,6 +198,8 @@ void registration(void)
 
     account_t userAccount;
 
+    int initialAccountBalance = 0;
+
     /* Gets new user information from user */
     printf("\nEnter First Name: ");
     scanf("%s", userAccount.first_name);
@@ -231,13 +215,14 @@ void registration(void)
     /* Note: does not check for duplicate usernames / firstname+last name combinations */
     if (newAccountID == 1)
     {
-        fprintf(log, "account_id,first_name,last_name,username,password\n");
+        fprintf(log, "account_id,first_name,last_name,username,password,account_balance\n");
     }
     fprintf(log, "%d,", newAccountID);
     fprintf(log, "%s,", userAccount.first_name);
     fprintf(log, "%s,", userAccount.last_name);
     fprintf(log, "%s,", userAccount.username);
-    fprintf(log, "%s", userAccount.password);
+    fprintf(log, "%s,", userAccount.password);
+    fprintf(log, "%d", initialAccountBalance);
     fprintf(log, "\n");
 
     fclose(log);
@@ -247,4 +232,105 @@ void registration(void)
 
     printf("\nPress enter to continue...");
     getchar();
+}
+
+void closeAccount();
+void depositFunds();
+void loanApplication();
+void transferFunds();
+void withdrawFunds();
+void balanceCheck();
+void logOut();
+
+int main(int argc, char *argv[])
+{
+    char option;
+
+    while (1)
+    {
+        if (userLoggedIn == true)
+        {
+            fflush(stdin);
+            // system("CLS");
+            printf("\nUser Account Menu");
+            printf("\n----------------");
+            printf("\n'B'alance check");
+            printf("\n'W'ithdraw");
+            printf("\n'D'eposit");
+            printf("\n'T'ransfer funds");
+            printf("\n'L'oan application");
+            printf("\n'C'lose account");
+            printf("\n'ESC'ape\n");
+            option = getchar();
+
+            /* 'C' == 67 */
+            if (option == 67)
+            {
+                // system("CLS");
+                // closeAccount();
+            }
+            /* 'D' == 68 */
+            else if (option == 68)
+            {
+                // system("CLS");
+                // depositFunds();
+            }
+            /* 'B' == 66 */
+            else if (option == 68)
+            {
+                // system("CLS");
+                // balanceCheck();
+            }
+            /* 'L' == 76 */
+            else if (option == 76)
+            {
+                // system("CLS");
+                // loanApplication();
+            }
+            /* 'T' == 84 */
+            else if (option == 84)
+            {
+                // system("CLS");
+                // transferFunds();
+            }
+            /* 'W' == 87 */
+            else if (option == 87)
+            {
+                // system("CLS");
+                // withdrawFunds();
+            }
+            /* 'ESC" == 27 */
+            else if (option == 27)
+            {
+                // system("CLS");
+                // logOut();
+            }
+        }
+        else // User is not logged in
+        {
+            fflush(stdin);
+            // system("CLS");
+            printf("\n    Main Menu\n----------------\n'1' to Register\n'2' to Login\n'Q' to Quit\n----------------\n");
+            option = getchar();
+
+            /* 49 == '1' */
+            if (option == 49)
+            {
+                // system("CLS");
+                registration();
+            }
+            /* 50 == '2' */
+            else if (option == 50)
+            {
+                // system("CLS");
+                login();
+            }
+            /* 81 == 'Q' */
+            else if (option == 81)
+            {
+                break;
+            }
+        }
+    }
+    return 0;
 }
